a513419f9d0fcfc3b0c394de8e63162a50a7141b
Abhigya Upadhyaya <u.abhigya07@gmail.com>
2026-07-31 21:12:53 +0545

    Added LPG Prices

--- DatePrices.py
+++ DatePrices.py
@@ -31,8 +31,9 @@ while True:
             date_text = cols[0].text.strip()
             petrol = cols[2].text.strip()
             diesel = cols[3].text.strip()
+            LPG = cols[5].text.strip()
 
-            scraped_data.append((date_text, petrol,diesel))
+            scraped_data.append((date_text, petrol, diesel, LPG))
 
     url_offset += 10
     time.sleep(1)
@@ -49,7 +50,9 @@ for entry in scraped_data:
             if datetime(2017, 1, 1).date() <= date_obj <= today:
                 cleaned_data.append({
                     "date": date_obj,
-                    "petrol_price": float(entry[1]), "diesel_price": float(entry[2])
+                    "petrol_price": float(entry[1]),
+                    "diesel_price": float(entry[2]),
+                    "LPG": float(entry[3])
                 })
 
         except ValueError:
@@ -64,19 +67,19 @@ start_date = cleaned_data[0]['date']
 end_date = today
 current_date = start_date
 
-price_map = {item['date']: (item['petrol_price'], item['diesel_price']) for item in cleaned_data}
+price_map = {item['date']: (item['petrol_price'], item['diesel_price'], item['LPG']) for item in cleaned_data}
 
 last_known_price = None
 
 with open("petrol_and_diesel_prices_filled.csv", "w", newline="") as f:
     writer = csv.writer(f)
 
-    writer.writerow(["Date", "Petrol_Price", "Diesel_Price"])
+    writer.writerow(["Date", "Petrol_Price", "Diesel_Price", "LPG"])
 
     while current_date <= end_date:
         if current_date in price_map:
             last_known_price = price_map[current_date]
 
-        writer.writerow([current_date, last_known_price[0], last_known_price[1]])
+        writer.writerow([current_date, last_known_price[0], last_known_price[1], last_known_price[2]])
 
         current_date += timedelta(days=1)
